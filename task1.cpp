ac4b8b4c688c2316a9296d96454dcce607df5d56
VeronikaKlimenko <V.Klymenko_FIT_2_22_B_d@knute.edu.ua>
2022-11-26 00:08:00 +0200

    1

---
+++ task1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+const float pi=3.14;
+int main()
+{
+  float kryg_s, kv_s, kv_p, r, a;
+  cout << "Vvedit s kryga " << endl ;
+  cin >> kryg_s ;
+  r = sqrt(kryg_s/pi);
+  a = r*2;
+  kv_s = pow(a,2);
+  kv_p = 4*a;
+  cout << "S kvadrata =" << kv_s << endl;
+  cout << "P kvadrata =" << kv_p << endl;
+  system ("pause");
+return 0;
+}
---
+++ task12.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double prod1, prod2, prod3, t, V;
+    cout << "Vvedit prodyktuvnist 1 trybu "  << endl ;
+    cin >> prod1; 
+    cout << "Vvedit prodyktuvnist 2 trybu "  << endl ;
+    cin >> prod2;
+    cout << "Vvedit prodyktuvnist 3 trybu "  << endl ;
+    cin >> prod3;
+    cout << "Vvedit chas robotu "  << endl ;
+    cin >> t;
+    V = (prod1+prod2+prod3)*t;
+    cout << "Voda v baseini " << V << endl ;
+    system ("pause");
+return 0;
+
+}
